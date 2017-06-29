8bf4b8c3bf39dca323d2aa1e6e00cf987cbaf52d
lindseygilmore <lindseygilmore21@gmail.com>
2017-06-29 09:19:27 -0500

    db.js updated

---
+++ blog/server/app.js
@@ -0,0 +1,16 @@
+//full stack!!//
+
+//what is express? a web framework written in node that allows us to build a web application
+var express = require('express'),
+	app 	= express(),
+	//http is part of node
+	server	= require('http')createServer(app),
+	//path is part of node
+	path 	= require('path'),
+	require('./db/db.js');
+
+server.listen(3000, function(){
+	console.log
+})
+
+
---
+++ blog/server/db/db.js
@@ -0,0 +1,18 @@
+//Step 1//
+
+var mongoose = require('mongoose');
+var connectionString = 'mongodb://localhost/blog2'
+
+mongoose.connect(connectionString);
+
+mongoose.connection.on('connected', function(){
+	console.log('connected to' + connectionString);
+})
+
+mongoose.connection.on('error', function(){
+	console.log('mongodb error' + error);
+})
+
+mongoose.connection.on('disconnected', function(){
+	console.log('Mongoose disconnected from' + connectionString);
+})
